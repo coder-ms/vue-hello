53b85f9f80f1d61b82f917089e61aba2afbfd127
coder-ms <marco.sbrascia@gmail.com>
2022-10-26 16:57:21 +0300

    Bonus

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
 
 console.log(Vue);
+// Create app
 const { createApp } = Vue;
 
 // 
@@ -16,12 +17,17 @@ const app = createApp({
         return {
             message: 'Capitain Jack Sparrow',
             myClass: 'red',
-            myImage:  ''
+            myImage1:  './img/PoC1.jpg',
+            myImage2:  './img/PoC2.jpg',
+            myImage3:  './img/PoC3.jpg',
+            myImage4:  './img/PoC4.jpg',
+            myImage5:  './img/PoC5.jpg'
         }
     },
 
-    // metodo Change color
+    // metodo 
     methods: {
+        //Change color
         changeColor() {
             // metodo 1
             this.myClass = this.myClass === 'red' ? 'blue' : 'red';
@@ -35,7 +41,25 @@ const app = createApp({
                 this.myClass = 'red';
             }
             */
+
+            // image
         },
+        //getPhrase 1 up to 6
+        getPhrase1(){
+            return 'Lo confesso. È mia intenzione requisire una di queste navi, prendere una ciurma a Tortuga, razziare, predare, saccheggiare finché le mie subdole membra cadranno';
+        },
+        getPhrase2(){
+            return 'No! Senza la chiave non apriamo quel che si apre con la chiave che lo apre, e a quale scopo scoprire qualcosa che rimarrebbe comunque chiuso, noi, non avendo, la suddetta chiave che invece lo aprirebbe avendola?!';
+        },
+        getPhrase3(){
+            return 'Signorine, volete gentilmente... shhh, basta. Ebbene sì, ti ho mentito. No, non ti amo. Sì, quel vestito ti ingrassa. Non sono mai stato a Bruxelles, e si pronuncia "egregio". Per inciso no, non ho conosciuto Pizarro ma adoro la sua pizza. E tutto precipita nella insignificanza alla luce del fatto che la mia nave, ancora una volta, è sparita. Comprendi?';
+        },
+        getPhrase4(){
+            return 'Posso capire tutto... Meno la parrucca.';
+        },
+        getPhrase5(){
+            return 'Non vado in cerca di guai! Che orribile stile di vita.';
+        }
     }
 });
 
